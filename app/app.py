0a35cf4bbeccdb1c04b6f1375afc1f7480bd54b8
Chia Yunhan <nigelchiayunhan@gmail.com>
2023-08-22 18:35:56 +1000

    auto reformatter reformatted

--- app/app.py
+++ app/app.py
@@ -12,37 +12,41 @@ from libs.base import *
 from libs.controls.ddcam import *
 
 import pygame
-        
+
+
 def app_init():
     """
-        Initializes the app
+    Initializes the app
     """
     print("App Initializing...")
-    
+
     # Initialize the pygame module
     pygame.init()
-    
+
     # Set the caption for the window
     pygame.display.set_caption("Friction in Design Project - AR")
-    
+
     # Initialise full screen
-    screen = pygame.display.set_mode((0,0), pygame.FULLSCREEN)
-    
+    screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+
     # Main loop (runs infinitely until window exits)
     controller = AppController(screen)
-    
+
     # Add initial controls
     controller.add_control(DDCamVisual(controller))
-    
+
     while controller.is_running():
         # Update all controls
         for control in controller.get_controls():
             control.update(controller)
-                   
+
         # Get all events from pygame, and exit if QUIT event exists.
         # Pass all events to controls.
         for event in pygame.event.get():
-            if event.type == pygame.QUIT or (event.type == pygame.KEYDOWN and event.key == pygame.KMOD_ALT | pygame.K_F4):
+            if event.type == pygame.QUIT or (
+                event.type == pygame.KEYDOWN
+                and event.key == pygame.KMOD_ALT | pygame.K_F4
+            ):
                 controller.exit()
                 break
             else:
@@ -52,28 +56,27 @@ def app_init():
         for control in controller.added_controls:
             controller.controls.append(control)
 
-
         for control in controller.removed_controls:
             controller.controls.remove(control)
-            
+
         # Update controller to clean state (no removed/added controls)
         controller.set_clean_state()
-        
+
         # Clear screen
-        screen.fill(pygame.Color(0,0,0))    
-            
+        screen.fill(pygame.Color(0, 0, 0))
+
         # Render all controls
         for control in controller.get_controls():
             control.render(controller, controller.screen)
-            
+
         # Update the screen
         pygame.display.flip()
-    
+
     # Release resources
     controller.camera.destroy()
-    
+
     print("App Exiting...")
-    
+
 
 if __name__ == "__main__":
     app_init()
--- app/libs/devices/camera.py
+++ app/libs/devices/camera.py
@@ -3,32 +3,33 @@ import cv2 as cv
 import pygame
 import threading
 
-class Camera():
+
+class Camera:
     """
-        This class represents the camera device that we will be using.
-        At this point in time, this camera class only supports webcam
-        feed (no depth camera).
-        
-        OpenCV is required.
+    This class represents the camera device that we will be using.
+    At this point in time, this camera class only supports webcam
+    feed (no depth camera).
+
+    OpenCV is required.
     """
-    
+
     def __init__(self):
         """
-            Creates a new camera device reference, 
-            which opens the camera for opencv and pygame use.
+        Creates a new camera device reference,
+        which opens the camera for opencv and pygame use.
         """
         try:
             # Create video capture from OpenCV (camera)
             self.loading = True
             self.valid = False
-            
+
             # Create thread for opening camera
             threading.Thread(target=self.open_camera, args=[]).start()
         except:
             print("Error creating thread (camera thread)")
             self.valid = False
             self.loading = False
-            
+
     def open_camera(self):
         try:
             print("Camera initializing...")
@@ -41,41 +42,38 @@ class Camera():
             print("Error initializing camera")
             self.valid = False
             self.loading = False
-            
+
     def capture_video(self):
         """
-            Captures the standard webcam footage.
+        Captures the standard webcam footage.
         """
         if self.valid:
-            ret, frame = self.video.read() # Get frame
-            
+            ret, frame = self.video.read()  # Get frame
+
             if not ret:
-                self.destroy() # Video cam error (or ended)
+                self.destroy()  # Video cam error (or ended)
 
             return frame
         return None
-    
+
     def capture_video_pygame(self) -> pygame.image:
         """
-            Captures the standard webcam footage, as a pygame
-            compatible image.
+        Captures the standard webcam footage, as a pygame
+        compatible image.
         """
         if self.valid:
             frame = self.capture_video()
             if frame is not None:
-                return pygame.image.frombuffer(frame.tobytes(), 
-                                               frame.shape[1::-1],
-                                               "BGR")
+                return pygame.image.frombuffer(
+                    frame.tobytes(), frame.shape[1::-1], "BGR"
+                )
         return None
-            
-                
-             
+
     def destroy(self):
         """
-            Releases the video capture reference.
+        Releases the video capture reference.
         """
         if self.valid:
             self.video.release()
 
         self.valid = False
-            
