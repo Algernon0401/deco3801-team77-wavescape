1faca51bbccede5eb75aa752d2a94219183b3084
noooogel <nigelchiayunhan@gmail.com>
2023-08-23 16:09:41 +1000

    Added test files for object detect/recognition

---
+++ test-spaces/nigel/main.py
@@ -0,0 +1,28 @@
+import cv2
+import matplotlib.pyplot as plt
+import numpy as np
+import joblib
+from mypackage import thing1
+
+
+yolov5s = r"yolov5s.joblib"
+yolov5l = r"yolov5l.joblib"
+# model = torch.hub.load("ultralytics/yolov5", "yolov5s")
+
+# joblib.dump(model, filename)
+loaded_model = joblib.load(yolov5l)
+
+
+cap = cv2.VideoCapture(0)
+
+while cap.isOpened():
+    ret, frame = cap.read()
+
+    results = loaded_model(frame)
+    cv2.imshow("webcam", np.squeeze(results.render()))
+
+    if cv2.waitKey(1) == ord("q"):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
