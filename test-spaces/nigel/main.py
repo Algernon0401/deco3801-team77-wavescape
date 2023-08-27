b8fe0ffa344dfdc6b8efe92fc81ad3a96df6bc9c
noooogel <nigelchiayunhan@gmail.com>
2023-08-28 00:59:43 +1000

    interactive window mess

--- test-spaces/nigel/main.py
+++ test-spaces/nigel/main.py
@@ -5,7 +5,7 @@ import torch
 
 
 yolov5s = torch.hub.load("ultralytics/yolov5", "yolov5s")
-yolov5l = torch.hub.load("ultralytics/yolov5", "yolov5l")
+# yolov5l = torch.hub.load("ultralytics/yolov5", "yolov5l")
 
 
 cap = cv2.VideoCapture(0)
@@ -13,7 +13,7 @@ cap = cv2.VideoCapture(0)
 while cap.isOpened():
     ret, frame = cap.read()
 
-    results = yolov5l(frame)
+    results = trained_model(frame)
     cv2.imshow("webcam", np.squeeze(results.render()))
 
     if cv2.waitKey(1) == ord("q"):
@@ -22,4 +22,27 @@ while cap.isOpened():
 cap.release()
 cv2.destroyAllWindows()
 
-!cd yolov5 && python train.py --batch 16 --epochs 3 --data config.yaml --weights yolov5s.pt --workers 2
+# !cd yolov5 && python train.py --batch 16 --epochs 3 --data config.yaml --weights yolov5s.pt --workers 2
+
+trained_model = torch.hub.load(
+    "ultralytics/yolov5",
+    "custom",
+    path=r"\yolov5\runs\train\exp2\weights\last.pt",
+    force_reload=True
+)
+
+import os
+img = os.path.join('data', 'images', 'train', 'earbuds12.jpg')
+results = trained_model(img)
+
+%matplotlib inline
+plt.imshow(np.squeeze(results.render()))
+plt.show()
+
+
+def find(name, path):
+    for root, dirs, files in os.walk(path):
+        if name in files:
+            return os.path.join(root, name)
+        
+find("last.pt",r"C:\vscode")
