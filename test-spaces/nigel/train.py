1191a1dcdabcda7e84a6fd34ad598e0e05351e4d
noooogel <nigelchiayunhan@gmail.com>
2023-09-06 14:58:50 +1000

    yaml file for kfold

--- test-spaces/nigel/train.py
+++ test-spaces/nigel/train.py
@@ -1,5 +1,4 @@
 from ultralytics import YOLO
-from sklearn.model_selection import KFold
 
 # if __name__ == "__main__":
 #     # Load a model
@@ -26,10 +25,7 @@ if __name__ == "__main__":
     model = YOLO("yolov8n.pt")  # load a pretrained model (recommended for training)
     # Use the model
 
-    ksplit = 5
-    kf = KFold(
-        n_splits=ksplit, shuffle=True, random_state=20
-    )  # setting random_state for repeatable results
+    ksplit = 5  # setting random_state for repeatable results
     ds_yamls = [
         r"datasets\full\2023-09-06_5-Fold_Cross-val\split_1\split_1_dataset.yaml",
         r"datasets\full\2023-09-06_5-Fold_Cross-val\split_2\split_2_dataset.yaml",
@@ -46,12 +42,13 @@ if __name__ == "__main__":
             data=dataset_yaml,
             imgsz=640,
             optimizer="Adam",
-            batch=-1,
+            batch=2,
             epochs=200,
             patience=30,
+            workers=2,
             device=0,
-            project="y8",
-            name="nano_w_kfold",
+            project="y8_nano_w_kfold",
+            name=f"{k} fold",
             exist_ok=True,
         )  # Include any training arguments
         results[k] = model.metrics  # save output metrics for further analysis
