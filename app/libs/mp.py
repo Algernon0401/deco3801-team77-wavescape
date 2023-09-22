673357203bb05e839465a4525a86956bdcb88c3d
samstk <samsticklen@outlook.com>
2023-09-22 14:23:28 +1000

    Multiprocessing working with YOLO

---
+++ app/libs/mp.py
@@ -0,0 +1,7 @@
+class Message:
+    """
+    Provides a simple message structure between processes
+    """
+    def __init__(self, type, data=0):
+        self.type = type
+        self.data = data
