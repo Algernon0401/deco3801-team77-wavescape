5c36c9873044e5838d2452f286d70b4341b07e5d
noooogel <nigelchiayunhan@gmail.com>
2023-10-11 17:04:08 +1000

    nigel test

--- test-spaces/nigel/test.py
+++ test-spaces/nigel/test.py
@@ -1,41 +1,17 @@
-from collections import defaultdict
+from ultralytics import YOLO
+from PIL import Image
 
-# Initialize the track_history dictionary
-track_history = defaultdict(list)
+# Load a pretrained YOLOv8n model
+model = YOLO(r"shapes_dataset\med_shapes\weights\best.pt")
 
-# Populate the track_history dictionary with sample data
-track_history["track1"] = [
-    (1.0, 2.0),
-    (2.0, 3.0),
-    (3.0, 4.0),
-    (1.0, 2.0),
-    (2.0, 3.0),
-    (3.0, 4.0),
-]  # Replace with your data
-track_history["track2"] = [(2.0, 3.0), (3.0, 4.0), (4.0, 5.0)]  # Replace with your data
+# Run inference on an image
+results = model(
+    r"C:\vscode\DECO3801\deco3801-team77\test-spaces\nigel\shapes_dataset\dataset2\valid\images\Screenshot-2023-10-10-17-41-12_png.rf.065dbde197dc62578aa16b5c06efa8bc.jpg"
+)  # results list
 
-# Dictionary to store the average coordinates for each track
-average_coordinates = {}
-
-# Calculate the average for each track
-for track_id, track in track_history.items():
-    if track:
-        total_x = sum(coord[0] for coord in track)
-        total_y = sum(coord[1] for coord in track)
-        avg_center_x = total_x / len(track)
-        avg_center_y = total_y / len(track)
-        avg_center = (avg_center_x, avg_center_y)
-        average_coordinates[track_id] = avg_center
-    else:
-        average_coordinates[track_id] = None  # Handle cases with no coordinates
-
-if len(track) > 2:  # keeps only the latest 30 records
-    track.pop(0)
-
-# Print the average coordinates for each track
-for track_id, avg_center in average_coordinates.items():
-    if avg_center is not None:
-        print(f"Average Center Coordinate for {track_id}: {avg_center}")
-        print(len(track))
-    else:
-        print(f"No coordinates for {track_id}.")
+# View results
+for r in results:
+    im_array = r.plot()  # plot a BGR numpy array of predictions
+    im = Image.fromarray(im_array[..., ::-1])  # RGB PIL image
+    im.show()  # show image
+    im.save("results.jpg")  # save image
